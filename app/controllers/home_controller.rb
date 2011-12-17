78446d07a5233c6df05281f19c70383cbf8265ae
Richard Huang <flyerhzm@gmail.com>
2011-12-17 19:17:53 +0800

    redirect to repository path if current_repository exist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in? && current_repository
+      redirect_to repository_path(current_repository) and return
+    end
   end
 end
