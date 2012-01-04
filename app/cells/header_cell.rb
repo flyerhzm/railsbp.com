ae21d9171b6e5583d0708e2c975c7814a134e415
Richard Huang <flyerhzm@gmail.com>
2012-01-04 20:45:45 +0800

    display visible repositories on header

--- app/cells/header_cell.rb
+++ app/cells/header_cell.rb
@@ -3,7 +3,7 @@ class HeaderCell < Cell::Rails
   def display(user, repository)
     @user = user
     @repository = repository
-    @repositories = @user.try(:repositories)
+    @repositories = @user.repositories.visible if @user.present?
     @repository = @repositories.first if @repository.nil? && @repositories.present?
     render
   end
