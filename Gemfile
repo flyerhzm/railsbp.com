f9848d463c5439bdbd2dafefce0cd4a1f33f247b
Richard Huang <flyerhzm@gmail.com>
2012-01-26 15:50:52 +0800

    notify remote proxy when repository configs updated

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'exception_notification'
 gem 'cancan'
 gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
 gem 'ckeditor'
+gem 'typhoeus'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/repository_configs.rb
+++ app/models/repository_configs.rb
@@ -22,9 +22,17 @@ class RepositoryConfigs
     File.open(@repository.config_file_path, 'w+') do |file|
       file.write(result.to_yaml)
     end
+    notify_remote(result.to_yaml)
   end
 
   def read
     YAML.load_file(@repository.config_file_path)
   end
+
+  def notify_remote(configs)
+    if @repository.update_configs_url?
+      Typhoeus::Request.post(@repository.update_configs_url, :params => {:configs => configs})
+    end
+  end
+  handle_asynchronously :notify_remote
 end
