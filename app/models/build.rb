7494b531387c387466c2049581583db875b0ea75
Richard Huang <flyerhzm@gmail.com>
2012-02-03 18:59:45 +0800

    no depth for git clone

--- app/models/build.rb
+++ app/models/build.rb
@@ -72,7 +72,7 @@ class Build < ActiveRecord::Base
     start_time = Time.now
     FileUtils.mkdir_p(analyze_path) unless File.exist?(analyze_path)
     FileUtils.cd(analyze_path)
-    g = Git.clone(repository.clone_url, repository.name, depth: 10)
+    g = Git.clone(repository.clone_url, repository.name)
     Dir.chdir(repository.name) { g.reset_hard(last_commit_id) }
     FileUtils.cp(repository.config_file_path, config_directory_path)
     rails_best_practices = RailsBestPractices::Analyzer.new("#{analyze_path}/#{repository.name}",
