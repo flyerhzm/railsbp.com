b4140d7fc8e8746724f36e2e1bbd5e0234aabf62
Richard Huang <flyerhzm@gmail.com>
2012-02-15 06:57:14 -0800

    use asset host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ RailsbpCom::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.railsbp.com"
+  config.action_controller.asset_host = "http://assets.railsbp.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
