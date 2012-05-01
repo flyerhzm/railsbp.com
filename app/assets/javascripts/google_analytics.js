29ab9bd8ed4555417b6c4cfd372756543f2809ac
Richard Huang <flyerhzm@gmail.com>
2012-05-01 10:29:45 +0800

    update google analytics

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,6 +1,8 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-28841644-1']);
+_gap.push(['_setSiteSpeedSampleRate', 100]);
 _gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageLoadTime']);
 
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
