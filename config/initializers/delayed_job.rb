827a275f19c976f886fd368926a010448e51006e
Richard Huang <flyerhzm@gmail.com>
2012-06-09 09:24:19 +0800

    no need to configure delayed_job

--- config/initializers/delayed_job.rb
+++
@@ -1,5 +0,0 @@
-# config/initializers/delayed_job_config.rb
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 60
-Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 5.minutes
