2fd3fb28b0fb9ffce688ad7e8b0cd718b532c5b5
Richard Huang <flyerhzm@gmail.com>
2013-02-03 16:25:04 +0800

    fix sign out issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ RailsbpCom::Application.routes.draw do
   post '/' => 'repositories#sync'
   post '/sync_proxy' => 'repositories#sync_proxy'
   root :to => "home#index"
-  devise_for :users, :controllers => { :sessions => "users/sessions", :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_scope :users do
-    get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", omniauth_callbacks: "users/omniauth_callbacks" }
   resources :repositories, only: [:show, :new, :create, :edit, :update] do
     resources :builds, only: [:show, :index]
     resource :configs, only: [:edit, :update] do
