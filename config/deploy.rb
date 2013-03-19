81a26ce3ecb5516aba226a05ece2d0384731bfb2
Richard Huang <flyerhzm@gmail.com>
2013-03-19 22:13:35 +0800

    update puma rails and newrelic_rpm

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,9 @@ require 'capistrano_colors'
 require 'bundler/capistrano'
 
 require 'rvm/capistrano'
-set :rvm_ruby_string, 'ruby-1.9.3-p194@railsbp.com'
+set :rvm_ruby_string, 'ruby-2.0.0-p0@railsbp.com'
+
+require 'puma/capistrano'
 
 set :application, "railsbp.com"
 set :repository,  "git@github.com:railsbp/railsbp.com.git"
@@ -25,6 +27,5 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     migrate
     cleanup
-    run "god restart railsbp-server"
   end
 end
