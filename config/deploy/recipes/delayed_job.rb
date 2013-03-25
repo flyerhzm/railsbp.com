17e2a1b844e6db54c1f462199f085c83d0f0b20a
Richard Huang <flyerhzm@gmail.com>
2013-03-25 22:51:28 +0800

    use monit to restart delayed_job

--- config/deploy/recipes/delayed_job.rb
+++ config/deploy/recipes/delayed_job.rb
@@ -1,7 +1,7 @@
-require "delayed/recipes"
+after  "deploy:restart", "delayed_job:restart"
 
-role :delayed_job, 'db.railsbp.com'
-set :delayed_job_server_role, :delayed_job
-
-before "deploy:restart", "delayed_job:stop"
-after  "deploy:restart", "delayed_job:start"
+namespace :delayed_job do
+  task :restart, :role => :db do
+    run "sudo monit restart delayed_job.railsbp.com"
+  end
+end
