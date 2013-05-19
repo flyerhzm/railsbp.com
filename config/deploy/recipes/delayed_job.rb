1e04452bf48bae118cf4ab39359c020726537127
Richard Huang <flyerhzm@gmail.com>
2013-05-19 09:47:48 +0800

    fix typo

--- config/deploy/recipes/delayed_job.rb
+++ config/deploy/recipes/delayed_job.rb
@@ -1,7 +1,7 @@
 after  "deploy:restart", "delayed_job:restart"
 
 namespace :delayed_job do
-  task :restart, :role => :db do
+  task :restart, :roles => :db do
     run "sudo monit restart delayed_job.railsbp.com"
   end
 end
