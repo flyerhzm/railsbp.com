4e9282653a0372098efeb574c820141073b3b4bc
Richard Huang <flyerhzm@gmail.com>
2014-01-29 13:06:19 +0800

    use current instead shared directory in puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,12 +1,12 @@
 environment 'production'
 daemonize
-pidfile '/home/deploy/sites/railsbp.com/production/shared/tmp/pids/puma.pid'
-state_path '/home/deploy/sites/railsbp.com/production/shared/tmp/sockets/puma.state'
-stdout_redirect '/home/deploy/sites/railsbp.com/production/shared/log/stdout.log', '/home/deploy/sites/railsbp.com/production/shared/log/stderr.log'
+pidfile '/home/deploy/sites/railsbp.com/production/current/tmp/pids/puma.pid'
+state_path '/home/deploy/sites/railsbp.com/production/current/tmp/sockets/puma.state'
+stdout_redirect '/home/deploy/sites/railsbp.com/production/current/log/stdout.log', '/home/deploy/sites/railsbp.com/production/current/log/stderr.log'
 threads 4, 16
 workers 2
-bind 'unix:///home/deploy/sites/railsbp.com/production/shared/tmp/sockets/puma.sock'
-activate_control_app 'unix:///home/deploy/sites/railsbp.com/production/shared/tmp/sockets/pumactl.sock'
+bind 'unix:///home/deploy/sites/railsbp.com/production/current/tmp/sockets/puma.sock'
+activate_control_app 'unix:///home/deploy/sites/railsbp.com/production/current/tmp/sockets/pumactl.sock'
 preload_app!
 
 on_worker_boot do
