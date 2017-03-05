93e6f687757b8ebf7a23de0468e177188de7faa9
Richard Huang <flyerhzm@gmail.com>
2017-03-05 20:08:34 +0800

    move public_repositories to helper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @repositories = Repository.visible.where("builds_count > 0").order("last_build_at desc").limit(10)
   end
 end
--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -2,4 +2,8 @@ module RepositoriesHelper
   def all_categories
     Category.includes(:configurations => :parameters).all
   end
+
+  def public_repositories
+    Repository.visible.where("builds_count > 0").order("last_build_at desc").limit(10)
+  end
 end
