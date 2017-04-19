3509f2c82fc4a7fd6d747a7f672bc358fcd97e27
Richard Huang <flyerhzm@gmail.com>
2017-04-19 13:03:24 +0800

    cleanup AnalyzeBuildJob

--- app/jobs/analyze_build_job.rb
+++ app/jobs/analyze_build_job.rb
@@ -3,17 +3,18 @@ class AnalyzeBuildJob < ActiveJob::Base
 
   def perform(build_id)
     build = Build.find(build_id)
+    repository = build.repository
     begin
       start_time = Time.now
       system("mkdir", "-p", build.analyze_path)
       Dir.chdir(build.analyze_path)
-      system("git", "clone", build.repository.clone_url)
-      system("rm", "#{build.repository.name}/.rvmrc")
-      Dir.chdir(build.repository.name)
+      system("git", "clone", repository.clone_url)
+      system("rm", "#{repository.name}/.rvmrc")
+      Dir.chdir(repository.name)
       system("git", "reset", "--hard", build.last_commit_id)
-      system("cp", build.repository.config_file_path, build.config_directory_path)
-      system("rails_best_practices --format yaml --silent --output-file #{build.yaml_output_file} --with-git #{build.analyze_path}/#{build.repository.name}")
-      RailsBestPractices::Core::Runner.base_path = File.join(build.analyze_path, build.repository.name)
+      system("cp", repository.config_file_path, build.config_directory_path)
+      system("rails_best_practices --format yaml --silent --output-file #{build.yaml_output_file} --with-git #{build.analyze_path}/#{repository.name}")
+      RailsBestPractices::Core::Runner.base_path = File.join(build.analyze_path, repository.name)
       build.current_errors.each do |error|
         error.highlight = (build.last_errors_memo[error.short_filename + error.message] != error.git_commit)
       end
@@ -22,7 +23,7 @@ class AnalyzeBuildJob < ActiveJob::Base
         file.puts eruby.evaluate(
           :errors         => build.current_errors,
           :github         => true,
-          :github_name    => build.repository.github_name,
+          :github_name    => repository.github_name,
           :last_commit_id => build.last_commit_id,
           :git            => true
         )
@@ -32,13 +33,13 @@ class AnalyzeBuildJob < ActiveJob::Base
       build.duration = end_time - start_time
       build.finished_at = end_time
       build.complete!
-      build.repository.touch(:last_build_at)
-      UserMailer.notify_build_success(build).deliver if build.repository.recipient_emails.present?
+      repository.touch(:last_build_at)
+      UserMailer.notify_build_success(build).deliver if repository.recipient_emails.present?
     rescue => e
       Rollbar.error(e)
       build.fail!
     ensure
-      system("rm", "-rf", "#{build.analyze_path}/#{build.repository.name}")
+      system("rm", "-rf", "#{build.analyze_path}/#{repository.name}")
     end
   end
 end
